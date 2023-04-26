4b753825614f9bbf674d8ad03b5e49d757ad4f28
Iuliia Dronova <jtyutereva@gmail.com>
2023-04-26 14:22:29 +0300

    Added liners and spacers

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,37 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.filled.AlternateEmail
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BusinessCardTheme {
+            BusinessCardTheme() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF0a3b54)
                 ) {
                     BusinessCardLayout()
                 }
@@ -45,7 +58,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCardLayout () {
-    ContactInformation(icon = painterResource(id = R.drawable.mail_48px), data = "jtyutereva@gmail.com")
+    Column {
+        IntroducingBlock(
+            image = painterResource(id = R.drawable.android_logo),
+            fullName = "Yulia Dronova",
+            title = "Software Developer",
+            modifier = Modifier.weight(3f)
+        )
+
+        ContactInformationBlock(modifier = Modifier.weight(1f))
+    }
+
 }
 
 @Composable
@@ -63,38 +86,82 @@ fun IntroducingBlock(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Image(painter = image, contentDescription = null)
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(150.dp)
+        )
         Text(
             text = fullName,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
+            fontSize = 36.sp,
+            color = Color(0xFFFFFFFF),
         )
         Text(
             text = title,
-            fontSize = 16.sp,
-            fontStyle = FontStyle.Italic
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3DDC84),
         )
     }
-
 }
 
 @Composable
-fun ContactInformation(
-    icon: Painter,
+fun ContactInformationBlock(modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Divider(color = Color.White, thickness = 1.dp)
+        Spacer(Modifier.weight(0.1f))
+
+        ContactInformationCard(
+            icon = Icons.Default.Mail,
+            data = "jtyutereva@gmail.com"
+        )
+
+        Spacer(Modifier.weight(0.1f))
+        Divider(color = Color.White, thickness = 1.dp)
+        Spacer(Modifier.weight(0.1f))
+
+        ContactInformationCard(
+            icon = Icons.Default.Phone,
+            data = "+31 6 XX XX XX"
+        )
+
+        Spacer(Modifier.weight(0.1f))
+        Divider(color = Color.White, thickness = 1.dp)
+        Spacer(Modifier.weight(0.1f))
+
+        ContactInformationCard(
+            icon = Icons.Default.AlternateEmail,
+            data = "someSocialMediaAccount"
+        )
+    }
+}
+
+@Composable
+private fun ContactInformationCard(
+    icon: ImageVector,
     data: String,
     modifier: Modifier = Modifier,
 ) {
     Row() {
         Icon(
-            painter = icon,
+            icon,
             contentDescription = null,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f),
+            Color(0xFF3DDC84)
         )
         Text(
             text = data,
-            modifier = Modifier.weight(4f))
+            modifier = Modifier.weight(4f),
+            Color(0xFFFFFFFF)
+        )
     }
-
 }
 
 @Preview(showBackground = true)
